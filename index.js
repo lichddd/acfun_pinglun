15604d5313bade0051cad7fa740efca0330ef1b8
lichddd <lichddd@126.com>
2017-08-07 16:56:06 +0800

    1

--- index.js
+++ index.js
@@ -4,10 +4,6 @@ import fs from 'fs'
 import getlist from './app/getlist'
 import getcomment from './app/getcomment'
 
-
-
-
-
 getl();
 async function getl() {
   var list = await getlist("http://search.aixifan.com/search?q=%E5%90%8C%E6%80%A7&pageSize=999999&parentChannelId=63&sortField=releaseDate");
